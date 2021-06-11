bba9adbc922e2025eb7bc24b4a063306a70a7a84
PedrooLska <pedrolucas81@hotmail.com>
2021-06-10 22:28:02 -0300

    Clear project

--- src/main.js
+++ src/main.js
@@ -1,14 +1,15 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
-import { createProvider } from './vue-apollo'
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import apolloProvider from './vue-apollo';
 
 Vue.config.productionTip = false;
 
 new Vue({
   router,
   store,
-  apolloProvider: createProvider(),
-  render: (h) => h(App)
-}).$mount("#app");
+  apolloProvider,
+  render: (h) => h(App),
+}).$mount('#app');
